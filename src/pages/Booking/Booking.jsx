39e37c82fc43fc5df0cfd13243e4a299994714ad
ruksat13 <mdruksathasanakib@gmail.com>
2026-05-30 22:42:54 +0600

    feat: email invoice working, remove debug logs

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -80,6 +80,7 @@ const Booking = () => {
 
         // Send email invoice
         try {
+          
             await emailjs.send(
                 import.meta.env.VITE_EMAILJS_SERVICE_ID,
                 import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
